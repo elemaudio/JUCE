c19a1ded55ba69ffc589c8d45e993e674e6481ea
hogliux <fabian.renn@gmail.com>
2022-02-16 11:49:00 +0100

    Add CMakeLists.txt example file to example GainPlugin instead of using a .jucer project file

--- examples/GainPlugin/Source/Main.cpp
+++ examples/GainPlugin/Source/Main.cpp
@@ -5,8 +5,19 @@
 
   ==============================================================================
 */
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_audio_plugin_client/juce_audio_plugin_client.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_core/juce_core.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_dsp/juce_dsp.h>
+#include <juce_events/juce_events.h>
+#include <juce_graphics/juce_graphics.h>
 
-#include "JuceHeader.h"
+using namespace juce;
+
 #include "GainPluginDemo.h"
 
 //==============================================================================
